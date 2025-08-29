6df9fa5b914b1590298bdbfeba8b54cfaa7ff5d7
derbenx <colin@derben.ca>
2025-08-28 23:19:10 -0400

    Update translate.js
    German Translated by FasteryXD

--- js/translate.js
+++ js/translate.js
@@ -1,12 +1,17 @@
 // Translated by google translate.
 
 const tran=[
-
+// Native
 ["English","Tree Name:","Given Names:","Family Name:","Gender:","Birth:","Death:","Other Info:","Relation:","Buttons:","Female","Male","Both","None","Choose","Married > ","Parental > ","Siblings > ","Adopted > ","Divorced > ","Unwed > ","Background Colour","Select Multiple","Translate:","ID Lines: select id pair and set relation on left.","Black","White","Red","Green","Blue","Orange","Yellow","Grey","The relation between ", " and ",". You can change this by using the relation drop down on the left.","New Person","Update Selected","Delete Selected","Fullscreen","Normal","New Tree","Save Tree","Load Tree","Center Tree","Hide","Show","undecided","If you want to clear this tree type:","Can't add person without relation, select someone first!","Can't delete person with sub branches or id:0","Enter a tree name first","Import GED", "This feature does not grab all data from GED files. Also, the data will need to be arranged manually.","Adopted: ","Christening: ","Cremation: ","Baptisim: ","Burial: ","Birth: ","Death: ","Note: ","Adopted.","Title: ","Help"],
-["Deutsch","Stammbaumname:", "Vorname:","Nachname:","Geschlecht:","Geburtstag:","Todestag:","Weitere Infos:","Verwandschaft:","Auswahl:","weiblich","männlich","divers","kein Geschlecht","Auswählen","verheiratet mit ", "Eltern von ","Geschwister von ","adoptiert von " "geschieden von ", "zusammenlebend mit ","Hintergrundfarbe","Mehrfachauswahl","Sprachauswahl:","ID-Paar links auswählen und Verwandschaft eintragen.","schwarz","weiß","rot","grün","blau","orange","gelb","grau","Die Verwandschaft zwischen "," und ",". Du kannst die Auswahl mit dem Verwandschafts-Auswahlfeld links ändern.","Neue Person","Person aktualisieren","Person löschen","Vollbildmodus","Normal","Neuer Stammbaum","Stammbaum speichern","Stammbaum laden","Stammbaum zentrieren","verstecken","anzeigen","unklar","Für den Fall, dass der Stammbaum geleert werden soll:","Person kann nicht ohne Verwandschaftsverhältnis angelegt werden. Verwandschaftsgrad auswählen!","Die Person mit id:0 kann nicht gelöscht werden","Stammbaumnamen vergeben!","GED importieren","Dieses Feature übernimmt nicht alle Daten aus einer GED-Datei. Die Daten müssen manuell angepasst werden.","Adoptiert: ","Getauft: ","Eingeäschert: ","Taufe: ","Beerdigung: ","Geburt: ","Tod: ","Notiz: ","adoptiert.","Titel: ","Hilfe"],
+
+// Translated by google translate.
 ["Francais","Nom de l'arbre :","Prénoms :","Nom de famille :","Sexe :","Naissance :","Décès :","Autres informations :","Relation :","Boutons :","Femme","Homme","Les deux","Aucun","Choisir","Marié > ","Parental > ","Frères et sœurs > ","Adopté > ","Divorcé > ","Célibataire > "," Couleur d'arrière-plan", "Sélectionner plusieurs", "Traduire :", "Lignes d'identification : sélectionner la paire d'identifiants et définir la relation à gauche.", "Noir", "Blanc", "Rouge", "Vert", "Bleu", "Orange","Jaune","Gris","La relation entre "," et ",". Vous pouvez modifier cela en utilisant la liste déroulante de relations sur la gauche.","Nouvelle personne","Mettre à jour la sélection"," Supprimer la sélection", "Plein écran", "Normal", "Nouvel arbre", "Enregistrer l'arborescence", "Charger l'arborescence", "Centrer l'arborescence", "Masquer", "Afficher", "Indécis","Si vous souhaitez effacer ce type d'arbre :","Impossible d'ajouter une personne sans relation, sélectionnez d'abord quelqu'un !","Impossible de supprimer une personne avec des sous-branches ou un identifiant :0","Entrez d'abord un nom d'arbre", "Importer GED", "Cette fonctionnalité ne récupère pas toutes les données des fichiers GED. De plus, les données devront être organisées manuellement.","Adopté : ","Baptême: ","Crémation: ","Baptême: ","Enterrement: ","Naissance: ","Décès: ","Note: ","Adopté.","Titre: ","Aide"],
 
+// Translated by google translate.
 ["Espanol","Nombre del árbol:","Nombres:","Apellido:","Género:","Nacimiento:","Muerte:","Otra información:","Relación:","Botones:","Mujer","Masculino","Ambos","Ninguno","Elegir","Casado > ","Paterno > ","Hermanos > ","Adoptado > ","Divorciado > ","Soltero > "," Color de fondo","Seleccionar varios","Traducir:","Líneas de identificación: seleccione el par de identificación y establezca la relación a la izquierda.","Negro","Blanco","Rojo","Verde","Azul"," Naranja","Amarillo","Gris","La relación entre ", " y ",". Puede cambiar esto usando el menú desplegable de relaciones a la izquierda.","Nueva persona","Actualizar seleccionado"," Eliminar seleccionados","Pantalla completa","Normal","Nuevo árbol","Guardar árbol","Cargar árbol","Centrar árbol","Ocultar","Mostrar","indeciso","Si desea borrar este tipo de árbol:","No se puede agregar una persona sin relación, ¡seleccione a alguien primero!","No se puede eliminar una persona con subramas o ID:0","Ingrese primero el nombre del árbol","Importar GED", " Esta función no captura todos los datos de los archivos GED. Además, los datos deberán organizarse manualmente.","Adoptado: ","Bautizo: ","Cremación: ","Bautismo: ","Entierro: "," Nacimiento: ","Defunción: ","Nota: ","Aprobado.","Título: ","Ayuda"]
+
+// Translated by FasteryXD
+["Deutsch","Stammbaumname:", "Vorname:","Nachname:","Geschlecht:","Geburtstag:","Todestag:","Weitere Infos:","Verwandschaft:","Auswahl:","weiblich","männlich","divers","kein Geschlecht","Auswählen","verheiratet mit ", "Eltern von ","Geschwister von ","adoptiert von " "geschieden von ", "zusammenlebend mit ","Hintergrundfarbe","Mehrfachauswahl","Sprachauswahl:","ID-Paar links auswählen und Verwandschaft eintragen.","schwarz","weiß","rot","grün","blau","orange","gelb","grau","Die Verwandschaft zwischen "," und ",". Du kannst die Auswahl mit dem Verwandschafts-Auswahlfeld links ändern.","Neue Person","Person aktualisieren","Person löschen","Vollbildmodus","Normal","Neuer Stammbaum","Stammbaum speichern","Stammbaum laden","Stammbaum zentrieren","verstecken","anzeigen","unklar","Für den Fall, dass der Stammbaum geleert werden soll:","Person kann nicht ohne Verwandschaftsverhältnis angelegt werden. Verwandschaftsgrad auswählen!","Die Person mit id:0 kann nicht gelöscht werden","Stammbaumnamen vergeben!","GED importieren","Dieses Feature übernimmt nicht alle Daten aus einer GED-Datei. Die Daten müssen manuell angepasst werden.","Adoptiert: ","Getauft: ","Eingeäschert: ","Taufe: ","Beerdigung: ","Geburt: ","Tod: ","Notiz: ","adoptiert.","Titel: ","Hilfe"],
 
 ];
 
@@ -53,18 +58,18 @@ help=[
    Haga clic, busque la primera identificación
    Haga doble clic, busque la segunda ID`,
 
- `  Pan lienzo: teclas WADS
+ `  Ansicht schwenken: WADS-Tasten
   
-  Modo normal:
-   Haga clic para seleccionar una persona
-   Arrastra para mover a la persona o desplazarte por el lienzo
+  Standardmodus: 
+   Klicken, um eine Person auszuwählen
+   Ziehen, um entweder eine Person zu verschieben oder die Ansicht schwenken.
   
-  Seleccione Múltiples:
-   Haz clic para alternar personas
-   Arrastra el lienzo para mover a las personas seleccionadas
+  Mehrfachauswahl:
+   Klicken, um Personen aus-/abzuwählen
+   Ansicht verschieben, um die ausgewählten Personen zu bewegen
    
-  Cuadro de líneas de identificación:
-   Haga clic, busque la primera identificación
-   Haga doble clic, busque la segunda identificación`
+  ID-Zeilen-Box:
+   Klicken, um die erste ID zu finden
+   Doppelklicken, um die zweite ID zu finden`
    
  ];
